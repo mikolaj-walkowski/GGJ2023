99ebf4bc1001cb4681605ea5a3f5938268b84a61
Sodment <sodment@gmail.com>
2023-02-04 23:31:22 +0100

    Add some graphs

--- main.gd
+++ main.gd
@@ -17,11 +17,12 @@ func _ready():
 	root.init(Vector2(0,0),null, Root.TileTypes.TOWN)
 	var road = rootV2.instance()
 	add_child(road)
-	generate_tree(7)
+	generate_tree(100)
 	road.init(Vector2(200, 200), root, Root.TileTypes.ROAD)
 	pass # Replace with function body.
 	
-
+func custom_sort(a: Vector2, b: Vector2):
+	return a.x < b.x
 
 func generate_tree(n: int):
 	var length: int = n - 2
@@ -32,7 +33,16 @@ func generate_tree(n: int):
 	randomize()
 	for i in range(length):
 		arr[i] = rng.randi_range(1, length+1)
-	var xd = get_tree_edges(arr, length)
+	var edges = get_tree_edges(arr, length)
+	edges.sort_custom(self, "custom_sort")
+	print(edges)
+	
+	#tree_nodes = generate_coordinates(edges)
+	
+func generate_coordinates(edges):
+	
+	for edge in edges:
+		pass
 		
 func get_tree_edges(prufer: Array, m: int):
 	var vertices = m + 2
@@ -69,6 +79,5 @@ func get_tree_edges(prufer: Array, m: int):
 		elif vertex_set[i] == 0 and jw == 1:
 			zupa_romana = i + 1
 	edges.append(Vector2(dupa_romana, zupa_romana))
-	print(edges)
 	return edges
 	
