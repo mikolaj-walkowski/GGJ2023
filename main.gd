8aa18973a69f91910d308e5098707bc3384f638a
Sodment <sodment@gmail.com>
2023-02-04 21:49:51 +0100

    Lofciam GDScript UwU Kawaiii

--- main.gd
+++ main.gd
@@ -21,4 +21,57 @@ func _ready():
 	var road = Roots[RootNames.ROAD].instance()
 	add_child(road)
 	road.init(Vector2(200, 200), root)
+	generate_tree(7)
 	pass # Replace with function body.
+	
+
+
+func generate_tree(n: int):
+	var length: int = n - 2
+	var arr = [] 
+	arr.resize(length)
+	arr.fill(0)
+	var rng = RandomNumberGenerator.new()
+	randomize()
+	for i in range(length):
+		arr[i] = rng.randi_range(1, length+1)
+	var xd = get_tree_edges(arr, length)
+		
+func get_tree_edges(prufer: Array, m: int):
+	var vertices = m + 2
+	var vertex_set = []
+	var edges = []
+	vertex_set.resize(vertices)
+	vertex_set.fill(0)
+	
+	for i in range(vertices - 2):
+		vertex_set[prufer[i] - 1] += 1
+		
+		
+	var jw = 0
+	for i in range(vertices - 2):
+		for j in range(vertices): 
+			jw = j
+			if (vertex_set[j] == 0):
+				 
+				# Remove from Prufer set and print
+				# pair.
+				vertex_set[j] = -1
+				edges.append(Vector2(j+1, prufer[i]))
+				vertex_set[prufer[i] - 1] -= 1
+				break
+	 
+	jw = 0
+	var dupa_romana = 0
+	var zupa_romana = 0
+	# For the last element
+	for i in range(vertices):
+		if (vertex_set[i] == 0 and jw == 0):
+			dupa_romana = i +1
+			jw += 1
+		elif vertex_set[i] == 0 and jw == 1:
+			zupa_romana = i + 1
+	edges.append(Vector2(dupa_romana, zupa_romana))
+	print(edges)
+	return edges
+	
